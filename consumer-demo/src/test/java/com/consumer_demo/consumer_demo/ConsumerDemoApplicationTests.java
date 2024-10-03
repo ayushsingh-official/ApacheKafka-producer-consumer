70dde72d589ca365cbf0822fe8fda028e7b7ba85
ayush <ayushofficial.in@gmail.com>
2024-10-03 16:10:52 +0530

    consumer-config

---
+++ consumer-demo/src/test/java/com/consumer_demo/consumer_demo/ConsumerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.consumer_demo.consumer_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
